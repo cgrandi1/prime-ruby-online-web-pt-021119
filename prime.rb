e97f2cc12ea0332b46188ef43de20b579b06107e
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-26 01:36:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def prime?(num)
-  range_number = (-100-..10000).to_a 
+  range_number = (-1..10000).to_a 
   range_number.each do |num|
     if num % num == 0 && num % 1 == 0 
       return true
   end 
-    return false
+     false 
   end 
 end
