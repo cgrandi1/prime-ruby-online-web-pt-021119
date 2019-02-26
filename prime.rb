fa5de9ff18d963a71246c54a01c041bf176d2cff
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-26 01:48:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ require 'pry'
 def prime?(num)
   range_number = (2..num-1).to_a 
   range_number.any? do |num|
-    if num % num == 0 || num % 1 == 0 
+    if num % num == 0 && num % 1 == 0 
       return true
   end 
      false 
